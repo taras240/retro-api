1d857dbe6185e2ad7df09ab14642de18197d79e1
taras240 <khomyn@outlook.com>
2024-02-13 17:19:04 +0200

    style

--- script.js
+++ script.js
@@ -2,20 +2,21 @@ const API_KEY = "iVoZ7p4a2IkZVdVEXbvwgeJbT7MkFiTh";
 const USER_NAME = "taras240";
 let gameId = "1532";
 const requestGameProgressScript = "API_GetGameInfoAndUserProgress.php";
-let wrapper = document.querySelector(".wrapper");
+let achivsSection = document.querySelector(".achivs_section");
 // y  =>  Your web API key.
 // z  =>  Your username.
 // u  =>  The target username.
 // g  =>  The target game ID.
 let url = `https://retroachievements.org/API/${requestGameProgressScript}?g=${gameId}&u=${USER_NAME}&z=${USER_NAME}&y=${API_KEY}`;
-fetch(url)
+fetch("achivs.json")
   .then((resp) => resp.json())
-  .then((resp) => parseGameAchievements(resp["Achievements"]));
+  .then((resp) => parseGameAchievements(resp));
 function parseGameAchievements(achivs) {
   console.log(achivs);
   Object.values(achivs).forEach((achiv) => {
     let achivElement = document.createElement("div");
-    achivElement.className = "achiv-block";
+    let isEarned = achiv.hasOwnProperty("DateEarned");
+    achivElement.className = `achiv-block ${isEarned ? "earned" : ""}`;
     achivElement.innerHTML = `
     <img
         class="achiv-preview"
@@ -29,6 +30,6 @@ function parseGameAchievements(achivs) {
         <p class="collected-date">${achiv["DateEarned"]}</p>
     </div>
     `;
-    wrapper.appendChild(achivElement);
+    achivsSection.appendChild(achivElement);
   });
 }
