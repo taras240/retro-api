d6e4b402c706fefaf9903abf5d9f8882d3b4e4c2
taras240 <khomyn@outlook.com>
2024-02-23 17:14:26 +0200

    first

---
+++ js/docElements.js
@@ -0,0 +1,155 @@
+class UI {
+  constructor() {
+    this.settings = document.querySelector(".prefs_section");
+    this.apiKey = document.querySelector("#api-key");
+    this.apiKey.value = userIdent.API_KEY;
+
+    this.login = document.querySelector("#login-input");
+    this.login.value = userIdent.USER_NAME;
+
+    this.columnsCount = document.querySelector("#columns");
+    this.columnWidth = document.querySelector("#columns-width");
+
+    this.updateInterval = document.querySelector("#update-time");
+    this.gameID = document.querySelector("#game-id");
+
+    this.gamePreview = document.querySelector("#game-preview");
+    this.gameTitle = document.querySelector("#game-title");
+    this.gamePlatform = document.querySelector("#game-platform");
+
+    this.achivsSection = document.querySelector(".achivs_section");
+    this.addEvents();
+  }
+  addEvents() {
+    this.apiKey.addEventListener("change", () => {
+      localStorage.setItem("apiKey", this.apiKey.value);
+      userIdent.API_KEY = this.apiKey.value;
+    });
+    this.login.addEventListener("change", () => {
+      localStorage.setItem("userName", this.login.value);
+      userIdent.USER_NAME = this.login.value;
+    });
+    this.columnsCount.addEventListener("change", () => {
+      this.achivsSection.style.gridTemplateColumns = `repeat(${this.columnsCount.value}, ${this.columnWidth.value}px)`;
+      // this.fitSizeVertically();
+    });
+    this.columnWidth.addEventListener("change", () => {
+      this.achivsSection.style.gridTemplateColumns = `repeat(${this.columnsCount.value}, ${this.columnWidth.value}px)`;
+    });
+    this.updateInterval.addEventListener("change", () => {
+      updateRateInSecs = this.updateInterval.value;
+    });
+    this.gameID.addEventListener("change", () => {
+      gameId = this.gameID.value;
+    });
+    document
+      .querySelector(".fit-width-button")
+      .addEventListener("click", () => this.fitSizeVertically());
+    document
+      .querySelector(".fit-columns-button")
+      .addEventListener("click", () => this.fitColumnsVertically());
+  }
+  parseGameAchievements(achivs) {
+    // console.log(achivs);
+    this.achivsSection.innerHTML = "";
+    this.updateGameInfo({
+      title: achivs.Title,
+      platform: achivs.ConsoleName,
+      imageIcon: achivs.ImageIcon,
+    });
+    Object.values(achivs.Achievements).forEach((achiv) => {
+      let achivElement = this.generateAchievement({
+        badgeName: achiv["BadgeName"],
+        title: achiv["Title"],
+        description: achiv["Description"],
+        dateEarned: achiv["DateEarned"],
+        dateHardEarned: achiv["DateEarnedHardcore"],
+      });
+      this.achivsSection.appendChild(achivElement);
+    });
+  }
+  updateGameInfo({ title, platform, imageIcon }) {
+    this.gamePreview.setAttribute(
+      "src",
+      `https://media.retroachievements.org${imageIcon}`
+    );
+    this.gameTitle.innerText = title;
+    this.gamePlatform.innerText = platform;
+  }
+  generateAchievement({
+    badgeName,
+    title,
+    description,
+    dateEarned,
+    dateHardEarned,
+  }) {
+    let achivElement = document.createElement("div");
+    let isEarned = dateEarned !== undefined;
+    let isHardcoreEarned = dateHardEarned !== undefined;
+    achivElement.className = `achiv-block ${isEarned ? "earned" : ""} ${
+      isHardcoreEarned ? "hardcore" : ""
+    }`;
+    achivElement.innerHTML = `
+    <div class="preview-container">
+        <img
+            class="achiv-preview"
+            src="https://media.retroachievements.org/Badge/${badgeName}.png"
+            alt=""
+            srcset=""
+        />
+    </div>
+    <div class="achiv-details">
+        <h2 class="achiv-title">${title}</h2>
+        <p class="achiv-description">${description}</p>
+        <p class="collected-date">${dateEarned}</p>
+    </div>
+    `;
+    achivElement.appendChild(
+      this.generateAchivDetails({
+        name: title,
+        description: description,
+        dateEarned: dateEarned,
+      })
+    );
+    return achivElement;
+  }
+  generateAchivDetails({ name, description, dateEarned }) {
+    let detailsElement = document.createElement("div");
+    detailsElement.className = "achiv-details-block";
+    detailsElement.innerHTML = `
+      <h2>${name}</h2>
+      <p>${description}</p>
+    `;
+    return detailsElement;
+  }
+  fitSizeVertically() {
+    let windowHeight = window.innerHeight;
+    while (this.achivsSection.clientHeight > windowHeight) {
+      this.achivsSection.style.gridTemplateColumns = `repeat(${
+        this.columnsCount.value
+      }, ${--this.columnWidth.value}px)`;
+    }
+
+    while (this.achivsSection.clientHeight + 10 < windowHeight) {
+      this.achivsSection.style.gridTemplateColumns = `repeat(${
+        this.columnsCount.value
+      }, ${++this.columnWidth.value}px)`;
+    }
+  }
+  fitColumnsVertically() {
+    let windowHeight = window.innerHeight;
+    while (this.achivsSection.clientHeight > windowHeight) {
+      console.log(this.columnsCount.value);
+      this.achivsSection.style.gridTemplateColumns = `repeat(${++this
+        .columnsCount.value}, ${this.columnWidth.value}px)`;
+    }
+
+    while (
+      this.achivsSection.clientHeight + this.columnWidth.value + 5 <
+      windowHeight
+    ) {
+      this.achivsSection.style.gridTemplateColumns = `repeat(${--this
+        .columnsCount.value}, ${this.columnWidth.value}px)`;
+    }
+  }
+}
---
+++ js/script.js
@@ -0,0 +1,59 @@
+const userIdent = {
+  API_KEY: localStorage.getItem("apiKey") ?? "",
+  USER_NAME: localStorage.getItem("userName") ?? "",
+};
+// userIdent.API_KEY = localStorage.getItem("apiKey");
+// userIdent.USER_NAME = localStorage.getItem("userName");
+let ui = new UI();
+let gameId = "1458";
+let updateRateInSecs = 5;
+let worker = setInterval(() => {});
+const gameIdElement = document.querySelector("#game-id");
+
+ui.gameID.value = gameId;
+
+const requestGameProgress = "API_GetGameInfoAndUserProgress.php";
+const requestUserRecentAchievements = "API_GetUserRecentAchievements.php";
+const requestGetUserProfile = "API_GetUserProfile.php";
+
+let url = `https://retroachievements.org/API/${requestGameProgress}?g=${gameId}&u=${userIdent.USER_NAME}&z=${userIdent.USER_NAME}&y=${userIdent.API_KEY}`;
+function generateUrl() {
+  // y  =>  Your web API key. // z  =>  Your username.// u  =>  The target username.// g  =>  The target game ID.
+  // let url = "achivs.json";
+  return `https://retroachievements.org/API/${requestGameProgress}?g=${gameId}&u=${userIdent.USER_NAME}&z=${userIdent.USER_NAME}&y=${userIdent.API_KEY}`;
+}
+updateAchivs(() => ui.fitSizeVertically());
+
+function updateAchivs(fun) {
+  fetch(generateUrl())
+    .then((resp) => resp.json())
+    .then((resp) => {
+      ui.parseGameAchievements(resp);
+      fun();
+    });
+  // console.log("tik");
+}
+
+function startWatching() {
+  document.querySelector("#start-watching").classList.add("hidden");
+  document.querySelector("#stop-watching").classList.remove("hidden");
+
+  updateAchivs();
+  worker = setInterval(() => {
+    updateAchivs();
+  }, updateRateInSecs * 1000);
+}
+
+function stopWatching() {
+  document.querySelector("#stop-watching").classList.add("hidden");
+  document.querySelector("#start-watching").classList.remove("hidden");
+  clearInterval(worker);
+}
+function closeSettings() {
+  ui.settings.classList.add("hidden");
+}
+function openSettings() {
+  ui.settings.classList.contains("hidden")
+    ? ui.settings.classList.remove("hidden")
+    : ui.settings.classList.add("hidden");
+}
