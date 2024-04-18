260a1a7756d86836e7de710097db12ce08323492
taras240 <khomyn@outlook.com>
2024-04-19 01:10:54 +0300

    small css improevements

--- js/ui.js
+++ js/ui.js
@@ -199,7 +199,7 @@ class UI {
       gameAchivsCount,
       richPresence,
     } = ui.statusPanel;
-
+    ui.gameCard.section.classList.remove("mastered");
     gamePreview.setAttribute(
       "src",
       `https://media.retroachievements.org${ImageIcon}`
@@ -1097,7 +1097,7 @@ class AchievementsBlock {
         <path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
       </svg>
     `;
-    previewContainer.appendChild(toTargetButton);
+    achivElement.appendChild(toTargetButton);
 
     // let achivDetails = this.generateAchivDetails(achievement);
     // achivElement.appendChild(achivDetails);
@@ -1533,6 +1533,9 @@ class StatusPanel {
       name: "Completion",
       value: `${completionByPointsPercents} of points [${completionByCountPercent}]`,
     });
+    if (totalPoints === totalEarnedPoints) {
+      ui.gameCard.section.classList.add("mastered");
+    }
     this.progressStatusText.innerText = completionByPointsPercents;
   }
 }
