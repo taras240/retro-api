0a4bb32837b4227c33eb2248974df2b53d480732
Taras <41308277+taras240@users.noreply.github.com>
2024-05-26 03:31:59 +0300

    Update ui-mobile.js

--- mobile/js/ui-mobile.js
+++ mobile/js/ui-mobile.js
@@ -426,7 +426,7 @@ class Home {
                     <div class="user-info__game-description" >
                         <h2 class="user-info__game-title">${game.Title}</h2>
                         <div class="game-stats__text">${game.ConsoleName}</div>
-                        <div  class="game-stats__button"  onclick="ui.expandGameItem(${game.ID},this); event.stopPropagation()">
+                        <div  class="game-stats__button"  onclick="ui.expandGameItem(${game.gameID},this); event.stopPropagation()">
                           <i class="game-stats__icon game-stats__expand-icon"></i>
                         </div>
                         <div class="user-info_game-stats-container">
@@ -698,7 +698,7 @@ class Awards {
                     </div>
                     <div class="awards__game-description" >
                         <h2 class="awards__game-title">${game.Title}</h2>
-                        <div  class="game-stats__button"  onclick="ui.expandGameItem(${game.ID},this); event.stopPropagation()">
+                        <div  class="game-stats__button"  onclick="ui.expandGameItem(${game.AwardData},this); event.stopPropagation()">
                           <i class="game-stats__icon game-stats__expand-icon"></i>
                         </div>
                         <div class="awards__game-stats__text">${game.ConsoleName}</div>
