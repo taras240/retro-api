a0a38423a3ef4c3fd71693232a6680e2e0a5e330
taras240 <khomyn@outlook.com>
2026-06-23 11:09:22 +0300

    upd

--- js/ui.js
+++ js/ui.js
@@ -47,7 +47,7 @@ export class UI {
   STICK_MARGIN = 1;
   STICK_TOLERANCE = 7;
   hltb = {};
-  isTest = true;
+  // isTest = true;
   constructor() {
     this.toggleLoading(false);
     this.initUI();
