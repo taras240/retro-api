5fb41ccd8a953177faeb463a90b5cae2bc92f5a9
taras240 <khomyn@outlook.com>
2026-06-26 10:39:17 +0300

    enhanced faq search

--- js/functions/games/search.js
+++ js/functions/games/search.js
@@ -15,9 +15,9 @@ export function openGameInRA({ ID }) {
 }
 export function searchFaqByGame({ Title, ConsoleName, ConsoleID }) {
     if (!Title) return;
-    ConsoleName ??= ConsoleID ? RA_PLATFORM_CODES[ConsoleID]?.Name.split("/")[0] : ""
+    ConsoleName ??= ConsoleID ? RA_PLATFORM_CODES[ConsoleID] : "";
     const query = encodeURIComponent(
-        `"${Title}" "${ConsoleName}" (${faqSearchQuery})`
+        `"${Title}" "${ConsoleName.split("/")[0]}" (${faqSearchQuery})`
     );
     searchQuery(query);
 }
